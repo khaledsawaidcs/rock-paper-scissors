0a39cab40fed197cca95108d0cea578fd3ce1a57
Khaled Sawad <khaled.sawaid.cs@gmail.com>
2024-06-23 12:07:36 +0300

    Updated and improved game code

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+# imported choice method, so I can pick a random element in an array (used in "get_computer_move"
 from random import choice
 
-# generate a random move for the computer
+
+# function that generates a random move (rock, paper or scissors) for the computer
 def get_computer_move():
     moves = ["r", "p", "s"]
     return choice(moves)
-
+# function the prompts the user for a move (has to be valid too)
 def get_user_move():
     while True:
         move = input("Chose your move! r-rock | p-paper | s-scissors - ").lower()
@@ -13,49 +15,38 @@ def get_user_move():
         print("Invalid input, try again")
     return move
 
+# function that checks who the winner is
+# prints the moves picked for each side
+# also prints who won, who lost and if draw
 def check_winner(computer, player):
-    dict = {"r" : "ROCK", "s" : "SCISSORS", "p": "PAPER"}
-    if computer == "r":
-        if player == "s":
-            print(f"Your move = {dict[player]} | Computer move = {dict[computer]}")
-            print("Unlucky, the computer won this round")
-            return 1
-        if player == "p":
-            print(f"Your move = {dict[player]} | Computer move = {dict[computer]}")
-            print("Nice! You won this round")
-            return 0
-        if player == "r":
-            print(f"Your move = {dict[player]} | Computer move = {dict[computer]}")
-            print("It's a draw")
-            return 2
-    if computer == "p":
-        if player == "r":
-            print(f"Your move = {dict[player]} | Computer move = {dict[computer]}")
-            print("Unlucky, the computer won this round")
-            return 1
-        if player == "s":
-            print(f"Your move = {dict[player]} | Computer move = {dict[computer]}")
-            print("Nice! You won this round")
-            return 0
-        if player == "p":
-            print(f"Your move = {dict[player]} | Computer move = {dict[computer]}")
-            print("It's a draw")
-            return 2
-    if computer == "s":
-        if player == "p":
-            print(f"Your move = {dict[player]} | Computer move = {dict[computer]}")
-            print("Unlucky, the computer won this round")
-            return 1
-        if player == "r":
-            print(f"Your move = {dict[player]} | Computer move = {dict[computer]}")
-            print("Nice! You won this round")
-            return 0
-        if player == "s":
-            print(f"Your move = {dict[player]} | Computer move = {dict[computer]}")
-            print("It's a draw")
-            return 2
+    current_move = (computer, player)
+
+    losing_message = "Unlucky, the computer won this round"
+    losing_options = [("r", "s"), ("s", "p"), ("p", "r")]
+
+    draw_message = "It's a draw!"
+    draw_options = [("r", "r"), ("s", "s"), ("p", "p")]
+
+    winning_message = "Nice! You won this round"
+
+    moves = {"r" : "ROCK", "s" : "SCISSORS", "p": "PAPER"}
+    show_moves = (f"Your move = {moves[player]} | Computer move = {moves[computer]}")
+
+    if current_move in losing_options:
+        print(show_moves)
+        print(losing_message)
+        return 1
+    if current_move in draw_options:
+        print(show_moves)
+        print(draw_message)
+        return 2
+    else:
+        print(show_moves)
+        print(winning_message)
+        return 0
 
 
+# function that makes the game start
 def game():
     print("Welcome To My Game!", end="")
     input("")
@@ -77,11 +68,10 @@ def game():
             user_wins += 1
         if winner == 1:
             computer_wins += 1
+
         print(f"SCORES: YOU = {user_wins} | COMPUTER = {computer_wins}")
 
-        if computer_wins == 3:
-            break
-        if user_wins == 3:
+        if computer_wins == 3 or user_wins == 3:
             break
 
     if user_wins == 3:
@@ -90,5 +80,6 @@ def game():
         print(":/ You lost")
 
 
-game()
+if __name__ == "__main__":
+    game()
 
